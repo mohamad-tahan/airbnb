1212d94bda19b6329c46d1675d73f88f1731cf4c
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-02 23:45:19 +0300

    Created admin page

---
+++ frontend-react/src/components/Admin/Admin.js
@@ -0,0 +1,22 @@
+import AddStay from "./AddStay";
+
+ function Admin(){
+
+
+
+    return(
+    <div>
+            <h1 class="adminHeader">Admin Panel</h1>
+            <AddStay/>
+
+        </div>
+
+
+
+    )
+
+
+
+}
+
+export default Admin;
