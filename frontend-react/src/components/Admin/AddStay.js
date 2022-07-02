34e2dc0c64d0ec3aa2329218d353a1d4c01c2bed
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-02 23:44:54 +0300

    Created AddStay function

---
+++ frontend-react/src/components/Admin/AddStay.js
@@ -0,0 +1,164 @@
+import { useEffect, useState } from "react";
+import FileBase64 from "react-file-base64";
+
+function AddStay() {
+  // Initialize Input State
+  const [name, setName] = useState("");
+  const [distance, setDistance] = useState("");
+  const [date, setDate] = useState("");
+  const [price, setPrice] = useState("");
+  const [rating, setRating] = useState("");
+  const [type, setType] = useState("");
+  const [pic1, setPic1] = useState("");
+  const [pic2, setPic2] = useState("");
+  const [pic3, setPic3] = useState("");
+  //console.log(pic1)
+
+//   console.log(name);
+//   console.log(price);
+//   console.log(type);
+//   console.log(pic1);
+
+  //Add Data to Backend on Submit
+  const access_token = localStorage.getItem("access_token");
+  const token_type = localStorage.getItem("token_type");
+  //   console.log(access_token);
+  //   console.log(token_type);
+
+  const Add = async () => {
+    
+    const res = await fetch("http://127.0.0.1:8000/api/add_stay", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token_type + access_token,
+      },
+      body: JSON.stringify({
+        name: name,
+        distance: distance,
+        date: date,
+        price: price,
+        rating: rating,
+        type: type,
+        pic1: pic1,
+        pic2: pic2,
+        pic3: pic3,
+      }),
+    }, alert("Stay Added"),
+     setName(""),
+    setDistance(""),
+    setDate(""),
+    setPrice(""),
+    setRating(""),
+    setType(""),
+    setPic1(""),
+    setPic2(""),
+    setPic3(""),);
+
+  };
+
+
+
+  const getFiles = (files) => {
+    setPic1(files[0].base64);  
+    setPic2(files[1].base64);
+    setPic3(files[2].base64);
+    
+  };
+
+  return (
+    <div className="addStay">
+      <div className="loginBox addStay" id="loginBox">
+      
+        <h6 className="title">Add Stay</h6>
+        <div className="inputBox">
+          <label className="logColor">Name</label>
+          <input
+            type="text"
+            value={name}
+            onInput={(e) => setName(e.target.value)}
+          />
+
+          <br />
+          <label className="logColor">Distance</label>
+          <input
+            type="text"
+            value={distance}
+            onInput={(e) => setDistance(e.target.value)}
+          />
+
+          <br />
+          <label className="logColor">Date</label>
+          <input
+            type="text"
+            value={date}
+            onInput={(e) => setDate(e.target.value)}
+          />
+
+          <br />
+          <label className="logColor">Price</label>
+          <input
+            type="text"
+            value={price}
+            onInput={(e) => setPrice(e.target.value)}
+          />
+
+          <br />
+          <label className="logColor">Rating</label>
+          <input
+            type="text"
+            value={rating}
+            onInput={(e) => setRating(e.target.value)}
+          />
+          <br />
+          <label className="logColor">Type</label>
+          <select
+            class="stayTypes"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option value="island">islands</option>
+            <option value="beach">Beach </option>
+            <option value="pools">Amazing pools</option>
+            <option value="omg">OMG!</option>
+            <option value="parks">National parks</option>
+            <option value="cabins">Cabins</option>
+            <option value="lakefront">Lakefront</option>
+            <option value="camping">Camping</option>
+            <option value="views">Amazing Views</option>
+            <option value="caves">Caves</option>
+          </select>
+          <br />
+          <br />
+          <label className="logColor">Add 3 Pictures</label>
+          <div className="pics">
+
+          <FileBase64
+          multiple={true}
+          onDone={(e) => {
+            getFiles(e);
+          }}
+        />
+
+
+
+
+            {/* <input type="file" value={pic1} onChange={(e) => setPic1(e.target.value)}/>
+          <input type="file"  value={pic2} onChange={(e) => setPic2(e.target.value)}/>
+          <input type="file" value={pic3}  onChange={(e) => setPic3(e.target.value)}/> */}
+          </div>
+
+          <button
+            className="log"
+            onClick={() => {
+              Add();
+            }}
+          >
+            Add Stay
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AddStay;
