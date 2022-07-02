f0940c5a5f808ff96edcafdf78f2b062d2afe538
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-02 23:43:31 +0300

    Adding pictures according to stay id

--- backend-laravel/app/Http/Controllers/AdminController.php
+++ backend-laravel/app/Http/Controllers/AdminController.php
@@ -9,19 +9,39 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function addStay(Request $request){
+        
         $stay = new Stay;
+     
         $stay->name = $request->name;
         $stay->distance = $request->distance;
         $stay->date = $request->date;
         $stay->price = $request->price;
         $stay->rating = $request->rating;
         $stay->type = $request->type;
-      
         
         $stay->save();
+
+            $pic1 = new Picture;
+            $pic1->stay_id = $stay->id;
+            $pic1->picture = $request->pic1;
+
+            $pic1->save();
+
+            $pic2 = new Picture;
+            $pic2->stay_id = $stay->id;
+            $pic2->picture = $request->pic2;
+
+            $pic2->save();
+            
+            $pic3 = new Picture;
+            $pic3->stay_id = $stay->id;
+            $pic3->picture = $request->pic3;
+            $pic3->save();
+        
         
         return response()->json([
-            "status" => "Stay Added"
+            "status" => "Stay Added",
+           
         ], 200);
     }
 
