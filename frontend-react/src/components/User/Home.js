f286a264fa38b7fda8efb17874f0afc5f46a5de2
mohamad-tahan <mohamadtahan880@gmail.com>
2022-07-04 02:46:50 +0300

    Modified Home

--- frontend-react/src/components/User/Home.js
+++ frontend-react/src/components/User/Home.js
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import Header from "./Header";
 import SlideShow from "./SlideShow";
 
+import { AiFillStar } from 'react-icons/ai';
+
+
 function Home({ type }) {
+    
 
     const[stays, setStays] = useState([])
    // console.log(stays)
@@ -12,32 +16,48 @@ function Home({ type }) {
     const res = await fetch("http://127.0.0.1:8000/api/getStays/" + type);
     const data = await res.json();
 
-    // console.log(res);
-  //   console.log(data.stays);
      setStays(data.stays);
-   //  console.log(data.stays[3].pictures[0].picture)
-   console.log(data.stays.pictures[0])
+  
 
     return data;
   };
+
+  const getAllStays = async () => {
+    const res = await fetch("http://127.0.0.1:8000/api/getAllStays");
+    const data = await res.json();
+
+     setStays(data.stays);
+  
+
+    return data;
+  };
+  
+
 useEffect(() => {
     getStays();
 },[type])
   
 
+
+useEffect(() => {
+    getStays();
+},[])
+  
+
   return (
   
   <div className="stays">
- {stays.map((stay,i) =>(
-        <div className="items">
+ {stays && stays.map((stay,i) =>(
+        <div className="items" key={i}>
 
             <SlideShow src = {stay.pictures}/>
             <div className="description">
-          <p className="info name" key={i}>{stay.name}</p>
-          <p className="info" key={i}>{stay.distance}</p>
-          <p className="info" key={i}>{stay.date}</p>
-          <p className="info price" key={i}>{stay.price}</p>
-          <p className="info rating" key={i}>{stay.rating}</p>
+            <p className="info rating" >{stay.rating} <sub><AiFillStar/></sub> </p>
+          <p className="info name" >{stay.name}</p>
+          <p className="info" >{stay.distance}</p>
+          <p className="info" >{stay.date}</p>
+          <p className="info price" >{stay.price}</p>
+          
         </div>
         </div>
     )
